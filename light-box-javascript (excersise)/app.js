07f3db40df186fc0e2c59fdd562193ba8a2a7749
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-07-15 16:23:32 +0700

    lightbox (finished) but the code is still not cleaned -> will do it later

--- light-box-javascript (excersise)/app.js
+++ light-box-javascript (excersise)/app.js
@@ -8,21 +8,55 @@ images.forEach((item) => {
 });
 
 function handleZoom(e) {
-  console.log(e.target);
   const imgSrc = e.target.getAttribute("src");
   const lightboxTemplate = `
-  <div class="lightbox">
+    <div class="lightbox">
+    <div class="lightbox-close">
+    <i class="fa-solid fa-xmark"></i>
+    </div>
         <div class="lightbox-content">
-            <img src="${imgSrc}"
-                alt="" class="lightbox-image">
+                <i class="fa-solid fa-angle-left lightbox-prev"></i>
+                <img src="${imgSrc}" alt="" class="lightbox-image">
+                <i class="fa-solid fa-angle-right lightbox-next"></i>
         </div>
     </div>
     `;
   document.body.insertAdjacentHTML("beforeend", lightboxTemplate);
 }
 
+let index = 0;
 document.body.addEventListener("click", function (e) {
+  const lightboxClose = document.querySelector(".lightbox-close");
+  if (!lightboxClose) return;
+  lightboxClose.addEventListener("click", function (e) {
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+  });
+  const lightboxImg = document.querySelector(".lightbox-image");
+  if (!lightboxImg) return;
+  let lightboxSrc = "";
   if (e.target.matches(".lightbox")) {
     e.target.parentNode.removeChild(e.target);
+  } else if (e.target.matches(".lightbox-next")) {
+    lightboxSrc = lightboxImg.getAttribute("src");
+    index = [...images].findIndex(
+      (item) => item.getAttribute("src") === lightboxSrc
+    );
+    index = index + 1;
+    if (index > images.length - 1) {
+      index = 0;
+    }
+    const newSrc = [...images][index].getAttribute("src");
+    lightboxImg.setAttribute("src", newSrc);
+  } else if (e.target.matches(".lightbox-prev")) {
+    lightboxSrc = lightboxImg.getAttribute("src");
+    index = [...images].findIndex(
+      (item) => item.getAttribute("src") === lightboxSrc
+    );
+    index = index - 1;
+    if (index < 0) {
+      index = images.length - 1;
+    }
+    const newSrc = [...images][index].getAttribute("src");
+    lightboxImg.setAttribute("src", newSrc);
   }
 });
