886b640283dc343c29fb0b7ee04dcfef38a626c8
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-10-26 16:31:04 +0700

    :100:: Finish creating todo-form with JS, no HTMLs included

--- todolist-class-javascript/app.js
+++ todolist-class-javascript/app.js
@@ -5,7 +5,28 @@ class Model {
 }
 
 class View {
-  constructor() {}
+  constructor() {
+    this.app = this.getElement("body");
+
+    this.todoWrapper = this.createElement("div", "todo");
+    this.todoForm = this.createElement("form", "todo-form");
+    this.todoForm.autoComplete = "off";
+
+    this.todoInput = this.createElement("input", "todo-input");
+    this.todoInput.type = "text";
+    this.todoInput.placeholder = "Enter your tasks...";
+    this.todoInput.name = "todo";
+
+    this.submitBtn = this.createElement("button", "todo-submit");
+    this.submitBtn.type = "submit";
+    this.submitBtn.textContent = "Add";
+
+    this.todoForm.append(this.todoInput, this.submitBtn);
+
+    this.todoList = this.createElement("div", "todo-list");
+    this.todoWrapper.append(this.todoForm, this.todoList);
+    this.app.append(this.todoWrapper);
+  }
   // createElement: Tạo ra element mong muốn
   createElement(tag, className) {
     const elm = document.createElement(tag);
